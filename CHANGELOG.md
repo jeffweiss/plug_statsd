96e7c82163a72e12ee4564b176d2ac73cb4d3d9f
jeffweiss <jeff.weiss@puppetlabs.com>
2015-05-15 00:07:35 -0700

    (#2) Allow configurable replacements for / and .
    Prior to this commit, we were sending the URI portions of the metric
    names off to statsd untransformed, which meant the metrics using
    contained a series of `/`s in them. This was fine for statsd, but
    graphite does not care for `/` in the metric name. Additionally, we
    sometimes have legitimate periods/full stops `.` as part of the URI, but
    this should not translate into a sub-portion of the metric; i.e.
    `/api/v1/users/jeff.weiss` becoming `api.v1.users.jeff.weiss` is
    confusing, therefore we first transform any period/full stop `.` into
    `_`, which would yield the clearer `api.v1.users.jeff_weiss` for the
    given example.
    We also allow the users to configure a replacement for `.` other than
    `_` and a replacement for `/` other than `.`.
    
    Close #2.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,6 @@
 
 0.2.0
  - Default sample rate changed from float 1.0 to integer 1
-
+ - Replace `.` in URI with `_` then replace `/` with `.`
+ - Allow configurable replacements for `.` and `/`
+ - Root URIs ( `/` ) now shown as `[root]`
--- lib/plug_statsd.ex
+++ lib/plug_statsd.ex
@@ -5,6 +5,8 @@ defmodule Plug.Statsd do
   @timing_sample_rate Application.get_env(:plug_statsd, :timing_sample_rate, @sample_rate)
   @request_sample_rate Application.get_env(:plug_statsd, :request_sample_rate, @sample_rate)
   @response_sample_rate Application.get_env(:plug_statsd, :response_sample_rate, @sample_rate)
+  @slash_replacement Application.get_env(:plug_statsd, :slash_replacement, ".")
+  @period_replacement Application.get_env(:plug_statsd, :period_replacement, "_")
 
   def init(opts), do: Keyword.merge(default_options, opts)
   def call(conn, opts) do
@@ -18,24 +20,42 @@ defmodule Plug.Statsd do
       end)
   end
 
+  def uri(conn, opts) do
+    Plug.Conn.full_path(conn)
+    |> sanitize_uri(opts)
+  end
+
+  defp sanitize_uri("/", opts) do
+    "[root]"
+  end
+  defp sanitize_uri("/"<>uri, opts) do
+    period_replacement = Keyword.get(opts, :period_replacement)
+    slash_replacement = Keyword.get(opts, :slash_replacement)
+
+    uri
+    |> String.replace(".", period_replacement)
+    |> String.replace("/", slash_replacement)
+  end
   defp default_options do
     [ sample_rate: @sample_rate,
       timing_sample_rate: @timing_sample_rate,
       request_sample_rate: @request_sample_rate,
       response_sample_rate: @response_sample_rate,
+      slash_replacement: @slash_replacement,
+      period_replacement: @period_replacement,
     ]
   end
 
   defp generalized_response_code(code) when is_integer(code), do: "#{div(code, 100)}xx"
   defp generalized_response_code(_code), do: "UNKNOWN"
 
-  defp metric_name(:response, conn) do
-    [:response, generalized_response_code(conn.status), conn.status, conn.method, Plug.Conn.full_path(conn)]
+  defp metric_name(:response, conn, opts) do
+    [:response, generalized_response_code(conn.status), conn.status, conn.method, uri(conn, opts)]
     |> List.flatten
     |> Enum.join(".")
   end
-  defp metric_name(type, conn) do
-    [type, conn.method, Plug.Conn.full_path(conn)]
+  defp metric_name(type, conn, opts) do
+    [type, conn.method, uri(conn, opts)]
     |> List.flatten
     |> Enum.join(".")
   end
@@ -51,13 +71,12 @@ defmodule Plug.Statsd do
   end
 
   defp send_metric(type = :timing, conn, opts, delay) do
-    name = metric_name(type, conn)
+    name = metric_name(type, conn, opts)
     ExStatsD.timer(delay, name, sample_rate: sample_rate(opts, type))
   end
   defp send_metric(type, conn, opts, _timing) do
     type
-    |> metric_name(conn)
+    |> metric_name(conn, opts)
     |> ExStatsD.increment(sample_rate: sample_rate(opts, type))
   end
-
 end
