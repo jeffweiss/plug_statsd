0d4883071d7ac0b579595133fc5030b3f18f7718
Jeff Weiss <jeff.weiss@puppetlabs.com>
2015-10-05 22:04:08 -0700

    bump to 0.3.2

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule PlugStatsd.Mixfile do
 
   def project do
     [app: :plug_statsd,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.0",
      name: "plug_statsd",
      description: @description,
